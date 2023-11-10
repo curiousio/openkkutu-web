e047b14e34a4a145c0b073efe9351c369fcd60f6
curiousio <kyeongmin@glowat.com>
2023-11-10 21:35:44 +0900

    Spring Initializr

---
+++ src/main/java/com/glowat/openkkutuweb/OpenkkutuWebApplication.java
@@ -0,0 +1,13 @@
+package com.glowat.openkkutuweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenkkutuWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenkkutuWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glowat/openkkutuweb/OpenkkutuWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glowat.openkkutuweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenkkutuWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
